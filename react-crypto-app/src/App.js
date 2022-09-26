6872336d0c3a9746a12e8dc0f9a2560d0a3a468b
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-26 15:34:45 +0200

    finished

--- react-crypto-app/src/App.js
+++ react-crypto-app/src/App.js
@@ -1,5 +1,49 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CoinComponent from "./components/CoinComponent";
+
 function App() {
-  return <div className="App"></div>;
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.coinstats.app/public/v1/coins?skip=0")
+      .then((response) => {
+        setListOfCoins(response.data.coins);
+      });
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
+  return (
+    <div className="App">
+      <div className="cryptoHeader">
+        <input
+          type="text"
+          placeholder="Search a crypto..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
+      </div>
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => {
+          return (
+            <CoinComponent
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
